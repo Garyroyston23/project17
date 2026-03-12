73c4f3eb000571430f5e7158151eabd1463cb8c2
Garyroyston23 <garyroystonwo@gmail.com>
2026-03-12 20:26:04 +0530

    Create DataObject class for handling datasets

---
+++ data/data_object.py
@@ -0,0 +1,8 @@
+class DataObject:
+
+    def __init__(self, X_train, X_test, y_train, y_test):
+
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
